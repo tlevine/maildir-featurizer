c30d2a5ad0729dc8aca845d5c2b807a3d8c96f79
Thomas Levine <_@thomaslevine.com>
2015-05-09 11:49:46 +0000

    -

--- maildir_featurizer.py
+++ maildir_featurizer.py
@@ -1,10 +1,27 @@
-import os, argparse, sys
+import pathlib, argparse, sys
 
 p = argparse.ArgumentParser(description = 'Get simple email features very quickly.')
 p.add_argument('maildir', nargs = '+', help = 'Maildir(s) to look at')
 
-def messages(_):
-    return [_]
+def features(p):
+    '''
+    :param pathlib.Path p: An email file
+    '''
+    return {
+        'date': datetime.datetime.fromtimestamp(int(p.name.split('_')[0])),
+    }
+
+def messages(maildir):
+    parent = pathlib.Path(maildir)
+    for child in parent.iterdir():
+        if child.is_file():
+            pass
+        elif child.name in {'new', 'cur', 'tmp'}:
+            for grandchild in child.iterdir():
+                if grandchild.is_file():
+                    yield features(grandchild)
+        else:
+            yield from messages(child)
 
 def cli():
     for maildir in p.parse_args().maildir:
