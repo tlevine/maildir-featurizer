c2ad9a2190c87610ae65c76bcf30d00425b7c253
Thomas Levine <_@thomaslevine.com>
2015-05-09 11:37:57 +0000

    -

--- maildir_featurizer.py
+++ maildir_featurizer.py
@@ -3,7 +3,12 @@ import os, argparse, sys
 p = argparse.ArgumentParser(description = 'Get simple email features very quickly.')
 p.add_argument('maildir', nargs = '+', help = 'Maildir(s) to look at')
 
+def messages(_):
+    return [_]
+
 def cli():
-    print(p.parse_args().maildir)
+    for maildir in p.parse_args().maildir:
+        for message in messages(maildir):
+            print(message)
 
 cli()
